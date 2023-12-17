41812e17c876c0bed92e4a6120ac2baf875182cc
Vlad Lysenko <me2ur.cat@yandex.ru>
2023-12-17 22:28:47 +0300

    Спринт 7. Проверка 5

--- test/managers/InFileBackedTaskManagerTest.java
+++ test/managers/InFileBackedTaskManagerTest.java
@@ -3,13 +3,8 @@ package managers;
 import manager.task.FileBackedTasksManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import task.Epic;
-import task.Subtask;
-import task.Task;
 
 import java.io.File;
-import java.time.Duration;
-import java.time.LocalDateTime;
 
 public class InFileBackedTaskManagerTest extends TaskManagerTest<FileBackedTasksManager> {
 
@@ -18,29 +13,6 @@ public class InFileBackedTaskManagerTest extends TaskManagerTest<FileBackedTasks
         manager = new FileBackedTasksManager(new File("tasks.csv"), null);
         manager.removeAllTask();
         manager.removeAllEpic();
-        task1 = new Task("Task name1", "Task desc1"
-                , LocalDateTime.parse("2023-12-30T18:44:06.456050"), Duration.ofHours(8));
-        task2 = new Task("Task name2", "Task desc2"
-                , LocalDateTime.parse("2023-12-31T18:44:06.456050"), Duration.ofHours(8));
-        task3 = new Task("Task name3", "Task desc3"
-                , LocalDateTime.parse("2024-01-01T18:44:06.456050"), Duration.ofHours(8));
-        task4 = new Task("Task name4", "Task desc4"
-                , LocalDateTime.parse("2024-01-02T18:44:06.456050"), Duration.ofHours(8));
-
-        epic1 = new Epic("Epic Name1", "Epic Desc1", LocalDateTime.parse("2024-01-04T18:44:06.456050"));
-        epic2 = new Epic("Epic Name2", "Epic Desc2", LocalDateTime.parse("2024-01-05T18:44:06.456050"));
-        epic3 = new Epic("Epic Name3", "Epic Desc3", LocalDateTime.parse("2024-01-06T18:44:06.456050"));
-
-        subtask1 = new Subtask("Subtask Name1", "Subtask Desc1", 1, LocalDateTime.parse("2024-01-07T18:44:06.456050"),
-                Duration.ofHours(10));
-        subtask2 = new Subtask("Subtask Name2", "Subtask Desc2", 1, LocalDateTime.parse("2024-01-08T18:44:06.456050"),
-                Duration.ofHours(10));
-        subtask3 = new Subtask("Subtask Name3", "Subtask Desc3", 1, LocalDateTime.parse("2024-01-09T18:44:06.456050"),
-                Duration.ofHours(10));
-        subtask4 = new Subtask("Subtask Name4", "Subtask Desc4", 2, LocalDateTime.parse("2024-01-10T18:44:06.456050"),
-                Duration.ofHours(10));
-        subtask5 = new Subtask("Subtask Name5", "Subtask Desc5", 3, LocalDateTime.parse("2024-01-11T18:44:06.456050"),
-                Duration.ofHours(10));
     }
 
     @AfterEach
--- test/managers/InMemoryTaskTest.java
+++ test/managers/InMemoryTaskTest.java
@@ -2,41 +2,11 @@ package managers;
 
 import manager.task.InMemoryTaskManager;
 import org.junit.jupiter.api.BeforeEach;
-import task.Epic;
-import task.Subtask;
-import task.Task;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
 
 public class InMemoryTaskTest extends TaskManagerTest<InMemoryTaskManager> {
 
     @BeforeEach
     public void init() {
         manager = new InMemoryTaskManager();
-
-        task1 = new Task("Task name1", "Task desc1"
-                , LocalDateTime.parse("2023-12-30T18:44:06.456050"), Duration.ofHours(8));
-        task2 = new Task("Task name2", "Task desc2"
-                , LocalDateTime.parse("2023-12-31T18:44:06.456050"), Duration.ofHours(8));
-        task3 = new Task("Task name3", "Task desc3"
-                , LocalDateTime.parse("2024-01-01T18:44:06.456050"), Duration.ofHours(8));
-        task4 = new Task("Task name4", "Task desc4"
-                , LocalDateTime.parse("2024-01-02T18:44:06.456050"), Duration.ofHours(8));
-
-        epic1 = new Epic("Epic Name1", "Epic Desc1", LocalDateTime.parse("2024-01-04T18:44:06.456050"));
-        epic2 = new Epic("Epic Name2", "Epic Desc2", LocalDateTime.parse("2024-01-05T18:44:06.456050"));
-        epic3 = new Epic("Epic Name3", "Epic Desc3", LocalDateTime.parse("2024-01-06T18:44:06.456050"));
-
-        subtask1 = new Subtask("Subtask Name1", "Subtask Desc1", 1, LocalDateTime.parse("2024-01-07T18:44:06.456050"),
-                Duration.ofHours(10));
-        subtask2 = new Subtask("Subtask Name2", "Subtask Desc2", 1, LocalDateTime.parse("2024-01-08T18:44:06.456050"),
-                Duration.ofHours(10));
-        subtask3 = new Subtask("Subtask Name3", "Subtask Desc3", 1, LocalDateTime.parse("2024-01-09T18:44:06.456050"),
-                Duration.ofHours(10));
-        subtask4 = new Subtask("Subtask Name4", "Subtask Desc4", 2, LocalDateTime.parse("2024-01-10T18:44:06.456050"),
-                Duration.ofHours(10));
-        subtask5 = new Subtask("Subtask Name5", "Subtask Desc5", 3, LocalDateTime.parse("2024-01-11T18:44:06.456050"),
-                Duration.ofHours(10));
     }
 }
