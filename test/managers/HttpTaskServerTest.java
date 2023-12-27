b55800a31c404aec533fc237de0a3bcec96e95af
Vlad Lysenko <me2ur.cat@yandex.ru>
2023-12-27 21:23:49 +0300

    Спринт 8. Проверка №1

--- test/managers/HttpTaskServerTest.java
+++ test/managers/HttpTaskServerTest.java
@@ -143,7 +143,7 @@ public class HttpTaskServerTest {
 
     @Test
     public void getTaskByIdTest() throws IOException, InterruptedException {
-        URI getTaskByIdUri = URI.create(HTTP_SERVER_URI + "task/?id=1");
+        URI getTaskByIdUri = URI.create(HTTP_SERVER_URI + "task/?id=4");
         URI addTaskUri = URI.create(HTTP_SERVER_URI + "task/");
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
@@ -169,14 +169,14 @@ public class HttpTaskServerTest {
                 .build();
         HttpResponse<String> response = httpClient.send(request, handler);
         Task task = gson.fromJson(response.body(), Task.class);
-        Assertions.assertEquals(task.getId(), 1);
+        Assertions.assertEquals(task.getId(), 4);
     }
 
     @Test
     public void deleteTaskByIdTest() throws IOException, InterruptedException {
         URI getAllTaskUri = URI.create(HTTP_SERVER_URI + "task/");
         URI addTaskUri = URI.create(HTTP_SERVER_URI + "task/");
-        URI deleteTaskByIdUri = URI.create(HTTP_SERVER_URI + "task/?id=1");
+        URI deleteTaskByIdUri = URI.create(HTTP_SERVER_URI + "task/?id=4");
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
         List.of(task1, task2, task3).forEach(it -> {
@@ -252,6 +252,309 @@ public class HttpTaskServerTest {
         List<Task> task = gson.fromJson(response.body(), new TypeToken<List<Task>>() {
         }.getType());
         Assertions.assertEquals(task.size(), 0);
+    }
+
+    @Test
+    public void getAllEpicTest() throws IOException, InterruptedException {
+        URI getAllEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        URI addEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        List.of(epic1, epic2, epic3).forEach(it -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(it)))
+                    .uri(addEpicUri)
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .header("Accept", "application/json")
+                    .build();
+            try {
+                httpClient.send(request, handler);
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(getAllEpicUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        List<Epic> epics = gson.fromJson(response.body(), new TypeToken<List<Epic>>() {
+        }.getType());
+        Assertions.assertEquals(epics.size(), 3);
+    }
+
+    @Test
+    public void getEpicByIdTest() throws IOException, InterruptedException {
+        URI getEpicByIdUri = URI.create(HTTP_SERVER_URI + "epic/?id=4");
+        URI addEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        List.of(epic1, epic2, epic3).forEach(it -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(it)))
+                    .uri(addEpicUri)
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .header("Accept", "application/json")
+                    .build();
+            try {
+                httpClient.send(request, handler);
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(getEpicByIdUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        Epic epic = gson.fromJson(response.body(), Epic.class);
+        Assertions.assertEquals(epic.getId(), 4);
+    }
+
+    @Test
+    public void deleteEpicByIdTest() throws IOException, InterruptedException {
+        URI getAllEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        URI addEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        URI deleteEpicByIdUri = URI.create(HTTP_SERVER_URI + "epic/?id=4");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        List.of(epic1, epic2, epic3).forEach(it -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(it)))
+                    .uri(addEpicUri)
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .header("Accept", "application/json")
+                    .build();
+            try {
+                httpClient.send(request, handler);
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .DELETE()
+                .uri(deleteEpicByIdUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        httpClient.send(request, handler);
+
+        request = HttpRequest.newBuilder()
+                .GET()
+                .uri(getAllEpicUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        List<Epic> epic = gson.fromJson(response.body(), new TypeToken<List<Epic>>() {
+        }.getType());
+        Assertions.assertEquals(epic.size(), 2);
+    }
+
+    @Test
+    public void deleteAllEpicTest() throws IOException, InterruptedException {
+        URI getAllEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        URI deleteAllEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        URI addEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        List.of(epic1, epic2, epic3).forEach(it -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(it)))
+                    .uri(addEpicUri)
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .header("Accept", "application/json")
+                    .build();
+            try {
+                httpClient.send(request, handler);
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .DELETE()
+                .uri(deleteAllEpicUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        httpClient.send(request, handler);
+
+        request = HttpRequest.newBuilder()
+                .GET()
+                .uri(getAllEpicUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        List<Epic> epics = gson.fromJson(response.body(), new TypeToken<List<Epic>>() {
+        }.getType());
+        Assertions.assertEquals(epics.size(), 0);
+    }
+
+    @Test
+    public void getAllSubtaskTest() throws IOException, InterruptedException {
+        URI addEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        URI addSubtaskUri = URI.create(HTTP_SERVER_URI + "subtask/");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        List.of(epic1, epic2, epic3).forEach(it -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(it)))
+                    .uri(addEpicUri)
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .header("Accept", "application/json")
+                    .build();
+            try {
+                httpClient.send(request, handler);
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(subtask1)))
+                .uri(addSubtaskUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        httpClient.send(request, handler);
+
+        request = HttpRequest.newBuilder()
+                .GET()
+                .uri(addSubtaskUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        List<Subtask> subtasks = gson.fromJson(response.body(), new TypeToken<List<Subtask>>() {
+        }.getType());
+        Assertions.assertEquals(subtasks.size(), 3);
+    }
+
+    @Test
+    public void getSubtaskByIdTest() throws IOException, InterruptedException {
+        URI addEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        URI addSubtaskUri = URI.create(HTTP_SERVER_URI + "subtask/");
+        URI getSubtaskUri = URI.create(HTTP_SERVER_URI + "subtask/?id=5");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        List.of(epic1, epic2, epic3).forEach(it -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(it)))
+                    .uri(addEpicUri)
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .header("Accept", "application/json")
+                    .build();
+            try {
+                httpClient.send(request, handler);
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(subtask1)))
+                .uri(addSubtaskUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        httpClient.send(request, handler);
+
+        request = HttpRequest.newBuilder()
+                .GET()
+                .uri(getSubtaskUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        Subtask subtask = gson.fromJson(response.body(), Subtask.class);
+        Assertions.assertEquals(subtask.getId(), 4);
+    }
+
+    @Test
+    public void deleteSubtaskByIdTest() throws IOException, InterruptedException {
+        URI addEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        URI addSubtaskUri = URI.create(HTTP_SERVER_URI + "subtask/");
+        URI deleteSubtaskUri = URI.create(HTTP_SERVER_URI + "subtask/?id=5");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        List.of(epic1, epic2, epic3).forEach(it -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(it)))
+                    .uri(addEpicUri)
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .header("Accept", "application/json")
+                    .build();
+            try {
+                httpClient.send(request, handler);
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(subtask1)))
+                .uri(addSubtaskUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        httpClient.send(request, handler);
+
+        request = HttpRequest.newBuilder()
+                .DELETE()
+                .uri(deleteSubtaskUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        List<Subtask> subtasks = gson.fromJson(response.body(), new TypeToken<List<Subtask>>() {
+        }.getType());
+        Assertions.assertEquals(subtasks.size(), 4);
+    }
+
+    @Test
+    public void deleteAllSubtaskTest() throws IOException, InterruptedException {
+        URI addEpicUri = URI.create(HTTP_SERVER_URI + "epic/");
+        URI addSubtaskUri = URI.create(HTTP_SERVER_URI + "subtask/");
+        URI deleteSubtaskUri = URI.create(HTTP_SERVER_URI + "subtask/");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        List.of(epic1, epic2, epic3).forEach(it -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(it)))
+                    .uri(addEpicUri)
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .header("Accept", "application/json")
+                    .build();
+            try {
+                httpClient.send(request, handler);
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(subtask1)))
+                .uri(addSubtaskUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        httpClient.send(request, handler);
+
+        request = HttpRequest.newBuilder()
+                .DELETE()
+                .uri(deleteSubtaskUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        List<Subtask> subtasks = gson.fromJson(response.body(), new TypeToken<List<Subtask>>() {
+        }.getType());
+        Assertions.assertEquals(subtasks.size(), 0);
     }
 
     @AfterAll
